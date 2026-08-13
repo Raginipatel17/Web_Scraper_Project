b2367bc351be23f13a75234e6f6603b6b89e06e8
raginiveloxn17 <ragini.k@veloxn.com>
2026-08-14 00:32:16 +0530

    add correction

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -8,8 +8,15 @@ async function eventscrape() {
 
   const listUrl = "https://www.eventbrite.com.au/d/australia--sydney/events/";
   const browser = await puppeteer.launch({
-  headless: true,
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+    headless: "new",
+    args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--no-zygote",
+        "--single-process"
+    ]
 });
   const page = await browser.newPage();
 
