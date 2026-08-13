27839f1207e6593c9bfec1c9a8d7a36992875130
raginiveloxn17 <ragini.k@veloxn.com>
2026-08-13 23:27:26 +0530

    corrected email post

--- client/src/form.jsx
+++ client/src/form.jsx
@@ -7,7 +7,7 @@ export default function TicketModal({ event, close }) {
   if(!event) return null;
   const submit=async(e)=>{
     e.preventDefault();
-    await axios.post("https://web-scraper-project-1.onrender.com//email",{
+    await axios.post("https://web-scraper-project-1.onrender.com/email",{
       email,
       consent,
       eventid:event._id,
